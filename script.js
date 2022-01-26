7a930ce324bdd69d4069050fdf8cbabcfced0cb8
Rubens Algeri <rubens_zys@hotmail.com>
2022-01-26 10:26:36 -0300

    seletor para sobremesa

---
+++ script.js
@@ -0,0 +1,76 @@
+function sele1(){
+    var element = document.getElementById("s1");
+    element.classList.add("borda-verde");
+    var element = document.getElementById("s2");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s3");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s4");
+    element.classList.remove("borda-verde");
+
+    var element = document.getElementById("icon-s1");
+    element.classList.remove("icon-none");
+    var element = document.getElementById("icon-s2");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s3");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s4");
+    element.classList.add("icon-none");
+}
+function sele2(){
+    var element = document.getElementById("s1");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s2");
+    element.classList.add("borda-verde");
+    var element = document.getElementById("s3");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s4");
+    element.classList.remove("borda-verde");
+
+    var element = document.getElementById("icon-s1");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s2");
+    element.classList.remove("icon-none");
+    var element = document.getElementById("icon-s3");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s4");
+    element.classList.add("icon-none");
+}
+function sele3(){
+    var element = document.getElementById("s1");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s2");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s3");
+    element.classList.add("borda-verde");
+    var element = document.getElementById("s4");
+    element.classList.remove("borda-verde");
+
+    var element = document.getElementById("icon-s1");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s2");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s3");
+    element.classList.remove("icon-none");
+    var element = document.getElementById("icon-s4");
+    element.classList.add("icon-none");
+}
+function sele4(){
+    var element = document.getElementById("s1");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s2");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s3");
+    element.classList.remove("borda-verde");
+    var element = document.getElementById("s4");
+    element.classList.add("borda-verde");
+
+    var element = document.getElementById("icon-s1");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s2");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s3");
+    element.classList.add("icon-none");
+    var element = document.getElementById("icon-s4");
+    element.classList.remove("icon-none");
+}
