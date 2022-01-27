1c3b1d1ee07a82b4749a8b73d41fba83e3e95acb
Rubens Algeri <rubens_zys@hotmail.com>
2022-01-27 10:23:59 -0300

    otimização de seletores

--- script.js
+++ script.js
@@ -1,228 +1,173 @@
-function selef1(){
-    var element = document.getElementById("f1");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("f2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f4");
-    element.classList.remove("borda-verde");
+// var selecionouComida = 0;
+// var selecionouBebida = 0;
+// var selecionouSobremesa = 0;
 
-    var element = document.getElementById("icon-f1");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-f2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f4");
-    element.classList.add("icon-none");
+
+function selef1(){
+    selecionouComida = true;
+
+    document.getElementById("f1").classList.add("borda-verde");
+    document.getElementById("f2").classList.remove("borda-verde");
+    document.getElementById("f3").classList.remove("borda-verde");
+    document.getElementById("f4").classList.remove("borda-verde");
+
+    document.getElementById("icon-f1").classList.remove("icon-none");
+    document.getElementById("icon-f2").classList.add("icon-none");
+    document.getElementById("icon-f3").classList.add("icon-none");
+    document.getElementById("icon-f4").classList.add("icon-none");
 }
 function selef2(){
-    var element = document.getElementById("f1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f2");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("f3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f4");
-    element.classList.remove("borda-verde");
+    selecionouComida = true;
 
-    var element = document.getElementById("icon-f1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f2");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-f3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f4");
-    element.classList.add("icon-none");
+    document.getElementById("f1").classList.remove("borda-verde");
+    document.getElementById("f2").classList.add("borda-verde");
+    document.getElementById("f3").classList.remove("borda-verde");
+    document.getElementById("f4").classList.remove("borda-verde");
+
+    document.getElementById("icon-f1").classList.add("icon-none");
+    document.getElementById("icon-f2").classList.remove("icon-none");
+    document.getElementById("icon-f3").classList.add("icon-none");
+    document.getElementById("icon-f4").classList.add("icon-none");
 }
 function selef3(){
-    var element = document.getElementById("f1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f3");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("f4");
-    element.classList.remove("borda-verde");
+    selecionouComida = true;
 
-    var element = document.getElementById("icon-f1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f3");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-f4");
-    element.classList.add("icon-none");
+    document.getElementById("f1").classList.remove("borda-verde");
+    document.getElementById("f2").classList.remove("borda-verde");
+    document.getElementById("f3").classList.add("borda-verde");
+    document.getElementById("f4").classList.remove("borda-verde");
+
+    document.getElementById("icon-f1").classList.add("icon-none");
+    document.getElementById("icon-f2").classList.add("icon-none");
+    document.getElementById("icon-f3").classList.remove("icon-none");
+    document.getElementById("icon-f4").classList.add("icon-none");
 }
 function selef4(){
-    var element = document.getElementById("f1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("f4");
-    element.classList.add("borda-verde");
+    selecionouComida = true;
 
-    var element = document.getElementById("icon-f1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-f4");
-    element.classList.remove("icon-none");
+    document.getElementById("f1").classList.remove("borda-verde");
+    document.getElementById("f2").classList.remove("borda-verde");
+    document.getElementById("f3").classList.remove("borda-verde");
+    document.getElementById("f4").classList.add("borda-verde");
+
+    document.getElementById("icon-f1").classList.add("icon-none");
+    document.getElementById("icon-f2").classList.add("icon-none");
+    document.getElementById("icon-f3").classList.add("icon-none");
+    document.getElementById("icon-f4").classList.remove("icon-none");
 }
 function seleb1(){
-    var element = document.getElementById("b1");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("b2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b4");
-    element.classList.remove("borda-verde");
+    selecionouBebida = true;
 
-    var element = document.getElementById("icon-b1");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-b2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b4");
-    element.classList.add("icon-none");
+    document.getElementById("b1").classList.add("borda-verde");
+    document.getElementById("b2").classList.remove("borda-verde");
+    document.getElementById("b3").classList.remove("borda-verde");
+    document.getElementById("b4").classList.remove("borda-verde");
+
+    document.getElementById("icon-b1").classList.remove("icon-none");
+    document.getElementById("icon-b2").classList.add("icon-none");
+    document.getElementById("icon-b3").classList.add("icon-none");
+    document.getElementById("icon-b4").classList.add("icon-none");
 }
 function seleb2(){
-    var element = document.getElementById("b1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b2");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("b3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b4");
-    element.classList.remove("borda-verde");
+    selecionouBebida = true;
 
-    var element = document.getElementById("icon-b1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b2");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-b3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b4");
-    element.classList.add("icon-none");
+    document.getElementById("b1").classList.remove("borda-verde");
+    document.getElementById("b2").classList.add("borda-verde");
+    document.getElementById("b3").classList.remove("borda-verde");
+    document.getElementById("b4").classList.remove("borda-verde");
+
+    document.getElementById("icon-b1").classList.add("icon-none");
+    document.getElementById("icon-b2").classList.remove("icon-none");
+    document.getElementById("icon-b3").classList.add("icon-none");
+    document.getElementById("icon-b4").classList.add("icon-none");
 }
 function seleb3(){
-    var element = document.getElementById("b1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b3");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("b4");
-    element.classList.remove("borda-verde");
+    selecionouBebida = true;
 
-    var element = document.getElementById("icon-b1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b3");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-b4");
-    element.classList.add("icon-none");
+    document.getElementById("b1").classList.remove("borda-verde");
+    document.getElementById("b2").classList.remove("borda-verde");
+    document.getElementById("b3").classList.add("borda-verde");
+    document.getElementById("b4").classList.remove("borda-verde");
+
+    document.getElementById("icon-b1").classList.add("icon-none");
+    document.getElementById("icon-b2").classList.add("icon-none");
+    document.getElementById("icon-b3").classList.remove("icon-none");
+    document.getElementById("icon-b4").classList.add("icon-none");
 }
 function seleb4(){
-    var element = document.getElementById("b1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("b4");
-    element.classList.add("borda-verde");
+    selecionouBebida = true;
 
-    var element = document.getElementById("icon-b1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-b4");
-    element.classList.remove("icon-none");
+    document.getElementById("b1").classList.remove("borda-verde");
+    document.getElementById("b2").classList.remove("borda-verde");
+    document.getElementById("b3").classList.remove("borda-verde");
+    document.getElementById("b4").classList.add("borda-verde");
+
+    document.getElementById("icon-b1").classList.add("icon-none");
+    document.getElementById("icon-b2").classList.add("icon-none");
+    document.getElementById("icon-b3").classList.add("icon-none");
+    document.getElementById("icon-b4").classList.remove("icon-none");
 }
 function seles1(){
-    var element = document.getElementById("s1");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("s2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s4");
-    element.classList.remove("borda-verde");
+    selecionouSobremesa = true;
 
-    var element = document.getElementById("icon-s1");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-s2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s4");
-    element.classList.add("icon-none");
+    document.getElementById("s1").classList.add("borda-verde");
+    document.getElementById("s2").classList.remove("borda-verde");
+    document.getElementById("s3").classList.remove("borda-verde");
+    document.getElementById("s4").classList.remove("borda-verde");
+
+    document.getElementById("icon-s1").classList.remove("icon-none");
+    document.getElementById("icon-s2").classList.add("icon-none");
+    document.getElementById("icon-s3").classList.add("icon-none");
+    document.getElementById("icon-s4").classList.add("icon-none");
 }
 function seles2(){
-    var element = document.getElementById("s1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s2");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("s3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s4");
-    element.classList.remove("borda-verde");
+    selecionouSobremesa = true;
 
-    var element = document.getElementById("icon-s1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s2");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-s3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s4");
-    element.classList.add("icon-none");
+    document.getElementById("s1").classList.remove("borda-verde");
+    document.getElementById("s2").classList.add("borda-verde");
+    document.getElementById("s3").classList.remove("borda-verde");
+    document.getElementById("s4").classList.remove("borda-verde");
+
+    document.getElementById("icon-s1").classList.add("icon-none");
+    document.getElementById("icon-s2").classList.remove("icon-none");
+    document.getElementById("icon-s3").classList.add("icon-none");
+    document.getElementById("icon-s4").classList.add("icon-none");
 }
 function seles3(){
-    var element = document.getElementById("s1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s3");
-    element.classList.add("borda-verde");
-    var element = document.getElementById("s4");
-    element.classList.remove("borda-verde");
+    selecionouSobremesa = true;
 
-    var element = document.getElementById("icon-s1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s3");
-    element.classList.remove("icon-none");
-    var element = document.getElementById("icon-s4");
-    element.classList.add("icon-none");
+    document.getElementById("s1").classList.remove("borda-verde");
+    document.getElementById("s2").classList.remove("borda-verde");
+    document.getElementById("s3").classList.add("borda-verde");
+    document.getElementById("s4").classList.remove("borda-verde");
+
+    document.getElementById("icon-s1").classList.add("icon-none");
+    document.getElementById("icon-s2").classList.add("icon-none");
+    document.getElementById("icon-s3").classList.remove("icon-none");
+    document.getElementById("icon-s4").classList.add("icon-none");
 }
 function seles4(){
-    var element = document.getElementById("s1");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s2");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s3");
-    element.classList.remove("borda-verde");
-    var element = document.getElementById("s4");
-    element.classList.add("borda-verde");
+    selecionouSobremesa = true;
 
-    var element = document.getElementById("icon-s1");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s2");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s3");
-    element.classList.add("icon-none");
-    var element = document.getElementById("icon-s4");
-    element.classList.remove("icon-none");
+    document.getElementById("s1").classList.remove("borda-verde");
+    document.getElementById("s2").classList.remove("borda-verde");
+    document.getElementById("s3").classList.remove("borda-verde");
+    document.getElementById("s4").classList.add("borda-verde");
+
+    document.getElementById("icon-s1").classList.add("icon-none");
+    document.getElementById("icon-s2").classList.add("icon-none");
+    document.getElementById("icon-s3").classList.add("icon-none");
+    document.getElementById("icon-s4").classList.remove("icon-none");
 }
+// var pedidoAberto = document.getElementById("2");
+// var fecharPedido = document.getElementById("3");
+
+// if(selecionouComida === true && selecionouBebida === true && selecionouSobremesa === true){
+//     pedidoAberto.classList.add("icon-none");
+//     fecharPedido.classList.remove("icon-none");
+// }
+// function confirmarPedido(){
+//     var confirmar = document.getElementById("1");
+//     confirmar.classList.remove("icon-none");
+//     document.getElementById("7").classList.add("fundo-apagado");
+// }
